c56e68274e4dfb2ec77e3d9712b073ffa047650f
Mark Pereira <markpereira17@gmail.com>
2019-06-13 14:42:46 +1000

    adding embedded view for popup signing view

---
+++ EmbeddedView.js
@@ -0,0 +1,24 @@
+import {
+  Text, View
+} from 'react-native';
+
+
+export default class SimpleView extends React.Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{fontSize: 20, color: 'white'}}>Embedded View</Text>
+      </View>
+    );
+  }
+};
+
+var styles = React.StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#9d48ff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import EmbeddedView from './EmbeddedView';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('EmbeddedView', () => EmbeddedView);
