32d08561716fbe2b0666d02dcfbc2b91e3e4f3b4
Hector Garcia <hmgaar@gmail.com>
2019-09-04 14:52:45 -0400

    Lint changes for src/Apps/DAOstack/Screens/DAOs.js

--- src/Apps/DAOstack/Screens/DAOs.js
+++ src/Apps/DAOstack/Screens/DAOs.js
@@ -1,31 +1,49 @@
-import React, {Component} from "react";
-import {Text, ScrollView, TouchableOpacity, StyleSheet, View, Dimensions, Image, ImageBackground} from "react-native";
-import { Query } from 'react-apollo'
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-use-before-define */
+/* eslint-disable global-require */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import {
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Dimensions,
+  Image,
+  ImageBackground,
+} from 'react-native';
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
-import { NavigationBar } from "../../../AliceCore/Components";
-import { DAOcolors } from "../Utils";
+import { NavigationBar } from '../../../AliceCore/Components';
+import { DAOcolors } from '../Utils';
 
 const { height, width } = Dimensions.get('window');
 
 const DAOS_QUERY = gql`
-      query {
-        daos(orderBy: reputationHoldersCount, orderDirection:desc) {
-          id
-          name
-          reputationHoldersCount
-          proposals {
-            id
-            stage
-          }
-        }
-    }`;
+  query {
+    daos(orderBy: reputationHoldersCount, orderDirection: desc) {
+      id
+      name
+      reputationHoldersCount
+      proposals {
+        id
+        stage
+      }
+    }
+  }
+`;
 
 export default class DAOs extends Component {
   static navigationOptions = ({ navigation }) => {
     const { navigate } = navigation;
     return {
-      header: null
+      header: null,
     };
   };
 
@@ -33,81 +51,249 @@ export default class DAOs extends Component {
     super(props);
     this.state = {
       loading: true,
-      daos: []
+      daos: [],
     };
-
   }
 
   render() {
     return (
-      <View style={{flex: 1, paddingTop: 50}}>
-        <NavigationBar/>
+      <View style={{ flex: 1, paddingTop: 50 }}>
+        <NavigationBar />
         <Query query={DAOS_QUERY}>
           {({ loading, error, data }) => {
             if (error) return <Text>Can't fetch DAOs</Text>;
-            if (loading) return <View style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: '#fff',
-            }}>
-              <Image source={require('../Assets/alchemy-logo-black.png')} style={{
-                width: 80,
-                marginBottom: 60,
-                resizeMode: 'contain',
-              }}/>
-              <Text style={{fontSize: 30, fontFamily: 'Didot'}}>Alchemy</Text>
-            </View>
+            if (loading) {
+              return (
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#fff',
+                  }}
+                >
+                  <Image
+                    source={require('../Assets/alchemy-logo-black.png')}
+                    style={{
+                      width: 80,
+                      marginBottom: 60,
+                      resizeMode: 'contain',
+                    }}
+                  />
+                  <Text style={{ fontSize: 30, fontFamily: 'Didot' }}>Alchemy</Text>
+                </View>
+              );
+            }
             return (
               <>
-                <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '100%', padding: 15}}>
-                  <Text style={{fontSize: 30, fontWeight: '700'}}>DAOs</Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    width: '100%',
+                    padding: 15,
+                  }}
+                >
+                  <Text style={{ fontSize: 30, fontWeight: '700' }}>DAOs</Text>
                 </View>
 
                 <ScrollView>
                   <View style={styles.container}>
                     {data.daos.map((dao, i) => {
-                      let {backgroundColor, color} = DAOcolors[i];
+                      const { backgroundColor, color } = DAOcolors[i];
                       if (i === 5) {
-                        return (<TouchableOpacity key={i} onPress={() => this.props.navigation.navigate('DAOstackHome', {dao, backgroundColor})} style={{...styles.cheezeDaoBox, ...styles.sharpShadow}}>
-                          <ImageBackground source={require('../Assets/cheeze-background.png')} style={{width: '100%', resizeMode: 'contain', paddingTop: 50, paddingBottom: 50, backgroundColor: 'yellow' }}>
-                            <Text style={{color: 'black', fontSize: 30, fontWeight: '700', fontFamily: 'Exocet', marginHorizontal: 20}}>CHeeze DAO</Text>
-                          </ImageBackground>
-                          <Image style={{width: width - 50, resizeMode: 'contain', margin: -15, marginHorizontal: 15}} source={require('../Assets/divider.png')} />
-                          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
-                            <View style={{alignItems: 'center', justifyContent: 'space-around', margin: 17}}>
-                              <Text style={{color: 'grey', fontSize: 10, marginBottom: 15, fontWeight: '700', fontFamily: 'Menlo-Regular'}}>Reputation Holders</Text>
-                              <Text style={{fontSize: 30, fontWeight: '700', fontFamily: 'Exocet'}}>{dao.reputationHoldersCount}</Text>
+                        return (
+                          <TouchableOpacity
+                            key={i}
+                            onPress={() =>
+                              this.props.navigation.navigate('DAOstackHome', {
+                                dao,
+                                backgroundColor,
+                              })
+                            }
+                            style={{ ...styles.cheezeDaoBox, ...styles.sharpShadow }}
+                          >
+                            <ImageBackground
+                              source={require('../Assets/cheeze-background.png')}
+                              style={{
+                                width: '100%',
+                                resizeMode: 'contain',
+                                paddingTop: 50,
+                                paddingBottom: 50,
+                                backgroundColor: 'yellow',
+                              }}
+                            >
+                              <Text
+                                style={{
+                                  color: 'black',
+                                  fontSize: 30,
+                                  fontWeight: '700',
+                                  fontFamily: 'Exocet',
+                                  marginHorizontal: 20,
+                                }}
+                              >
+                                CHeeze DAO
+                              </Text>
+                            </ImageBackground>
+                            <Image
+                              style={{
+                                width: width - 50,
+                                resizeMode: 'contain',
+                                margin: -15,
+                                marginHorizontal: 15,
+                              }}
+                              source={require('../Assets/divider.png')}
+                            />
+                            <View
+                              style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                justifyContent: 'space-around',
+                              }}
+                            >
+                              <View
+                                style={{
+                                  alignItems: 'center',
+                                  justifyContent: 'space-around',
+                                  margin: 17,
+                                }}
+                              >
+                                <Text
+                                  style={{
+                                    color: 'grey',
+                                    fontSize: 10,
+                                    marginBottom: 15,
+                                    fontWeight: '700',
+                                    fontFamily: 'Menlo-Regular',
+                                  }}
+                                >
+                                  Reputation Holders
+                                </Text>
+                                <Text
+                                  style={{ fontSize: 30, fontWeight: '700', fontFamily: 'Exocet' }}
+                                >
+                                  {dao.reputationHoldersCount}
+                                </Text>
+                              </View>
+                              <View style={{ height: 50, width: 1, backgroundColor: '#c9c9c9' }} />
+                              <View
+                                style={{
+                                  alignItems: 'center',
+                                  justifyContent: 'space-around',
+                                  margin: 17,
+                                }}
+                              >
+                                <Text
+                                  style={{
+                                    color: 'grey',
+                                    fontSize: 10,
+                                    marginBottom: 15,
+                                    fontWeight: '700',
+                                    fontFamily: 'Menlo-Regular',
+                                  }}
+                                >
+                                  Open Proposals
+                                </Text>
+                                <Text
+                                  style={{ fontSize: 30, fontWeight: '700', fontFamily: 'Exocet' }}
+                                >
+                                  {
+                                    dao.proposals.filter(
+                                      proposal =>
+                                        proposal.stage !== 'Executed' &&
+                                        proposal.stage !== 'ExpiredInQueue'
+                                    ).length
+                                  }
+                                </Text>
+                              </View>
                             </View>
-                            <View style={{height: 50, width: 1, backgroundColor: '#c9c9c9'}}/>
-                            <View style={{alignItems: 'center', justifyContent: 'space-around', margin: 17}}>
-                              <Text style={{color: 'grey', fontSize: 10, marginBottom: 15, fontWeight: '700', fontFamily: 'Menlo-Regular'}}>Open Proposals</Text>
-                              <Text style={{fontSize: 30, fontWeight: '700', fontFamily: 'Exocet'}}>{dao.proposals.filter(proposal => proposal.stage !== "Executed" && proposal.stage !== "ExpiredInQueue" ).length}</Text>
-                            </View>
-                          </View>
-                        </TouchableOpacity>)
-
+                          </TouchableOpacity>
+                        );
                       }
                       return (
-                        <TouchableOpacity key={i} onPress={() => this.props.navigation.navigate('DAOstackHome', {dao, backgroundColor})} style={styles.daoBox}>
-                          <View style={{width: '100%', padding: 30, paddingTop: 50, paddingBottom: 50, borderTopLeftRadius: 15, borderTopRightRadius: 15, backgroundColor }}>
-                            <Text style={{color, fontSize: 20, fontWeight: '700'}}>{dao.name}</Text>
+                        <TouchableOpacity
+                          key={i}
+                          onPress={() =>
+                            this.props.navigation.navigate('DAOstackHome', { dao, backgroundColor })
+                          }
+                          style={styles.daoBox}
+                        >
+                          <View
+                            style={{
+                              width: '100%',
+                              padding: 30,
+                              paddingTop: 50,
+                              paddingBottom: 50,
+                              borderTopLeftRadius: 15,
+                              borderTopRightRadius: 15,
+                              backgroundColor,
+                            }}
+                          >
+                            <Text style={{ color, fontSize: 20, fontWeight: '700' }}>
+                              {dao.name}
+                            </Text>
                           </View>
-                          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
-                            <View style={{alignItems: 'center', justifyContent: 'space-around', margin: 17}}>
-                              <Text style={{color: 'grey', fontSize: 10, marginBottom: 15, fontWeight: '700'}}>Reputation Holders</Text>
-                              <Text style={{fontSize: 25, fontWeight: '700'}}>{dao.reputationHoldersCount}</Text>
+                          <View
+                            style={{
+                              flexDirection: 'row',
+                              alignItems: 'center',
+                              justifyContent: 'space-around',
+                            }}
+                          >
+                            <View
+                              style={{
+                                alignItems: 'center',
+                                justifyContent: 'space-around',
+                                margin: 17,
+                              }}
+                            >
+                              <Text
+                                style={{
+                                  color: 'grey',
+                                  fontSize: 10,
+                                  marginBottom: 15,
+                                  fontWeight: '700',
+                                }}
+                              >
+                                Reputation Holders
+                              </Text>
+                              <Text style={{ fontSize: 25, fontWeight: '700' }}>
+                                {dao.reputationHoldersCount}
+                              </Text>
                             </View>
-                            <View style={{height: 50, width: 1, backgroundColor: '#c9c9c9'}}/>
-                            <View style={{alignItems: 'center', justifyContent: 'space-around', margin: 17}}>
-                              <Text style={{color: 'grey', fontSize: 10, marginBottom: 15, fontWeight: '700'}}>Open Proposals</Text>
-                              <Text style={{fontSize: 25, fontWeight: '700'}}>{dao.proposals.filter(proposal => proposal.stage !== "Executed" && proposal.stage !== "ExpiredInQueue" ).length}</Text>
+                            <View style={{ height: 50, width: 1, backgroundColor: '#c9c9c9' }} />
+                            <View
+                              style={{
+                                alignItems: 'center',
+                                justifyContent: 'space-around',
+                                margin: 17,
+                              }}
+                            >
+                              <Text
+                                style={{
+                                  color: 'grey',
+                                  fontSize: 10,
+                                  marginBottom: 15,
+                                  fontWeight: '700',
+                                }}
+                              >
+                                Open Proposals
+                              </Text>
+                              <Text style={{ fontSize: 25, fontWeight: '700' }}>
+                                {
+                                  dao.proposals.filter(
+                                    proposal =>
+                                      proposal.stage !== 'Executed' &&
+                                      proposal.stage !== 'ExpiredInQueue'
+                                  ).length
+                                }
+                              </Text>
                             </View>
                           </View>
                         </TouchableOpacity>
-                      )
-                    })
-                    }
+                      );
+                    })}
                   </View>
                 </ScrollView>
               </>
@@ -128,16 +314,19 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10
+    margin: 10,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5
+    marginBottom: 5,
   },
   roundedProfileImage: {
-    width:100, height:100, borderWidth:3,
-    borderColor:'white', borderRadius:50
+    width: 100,
+    height: 100,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 50,
   },
   daoBox: {
     width: width - 20,
@@ -174,7 +363,5 @@ const styles = StyleSheet.create({
     },
     shadowRadius: 0,
     shadowOpacity: 1,
-
   },
-
 });
