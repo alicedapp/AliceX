4b2f9d13c45fc48918ee453ddb165cf4c3c35c68
Hector Garcia <hmgaar@gmail.com>
2019-09-04 17:20:44 -0400

    Lint changes for src/Apps/DAOstack/Components/FloatingButton.js. Only issue to fix: 'type arguments' can only be used in a .ts file.

--- src/Apps/DAOstack/Components/FloatingButton.js
+++ src/Apps/DAOstack/Components/FloatingButton.js
@@ -1,45 +1,62 @@
-import React, { Component } from "react";
-import { Animated, Image, StyleSheet, TouchableWithoutFeedback } from "react-native";
-
+/* eslint-disable no-undef */
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import { Animated, Image, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 
 export default class FloatingButton extends Component<Props> {
   constructor(props) {
     super(props);
 
     this.state = {
-      animatePress: new Animated.Value(1)
+      animatePress: new Animated.Value(1),
     };
-
   }
 
   animateIn = () => {
     Animated.timing(this.state.animatePress, {
       toValue: 0.95,
-      duration: 50
+      duration: 50,
     }).start();
   };
 
   animateOut = () => {
     Animated.timing(this.state.animatePress, {
       toValue: 1,
-      duration: 50
+      duration: 50,
     }).start();
-
   };
 
   render() {
-    const {tokenInfo, iterator, token} = this.props;
+    const { tokenInfo, iterator, token } = this.props;
     return (
-      <TouchableWithoutFeedback {...this.props} key={iterator} onPressIn={this.animateIn} onPressOut={this.animateOut}>
-        <Animated.View  style={{...styles.button, ...this.props.style, position: 'absolute', bottom: 30, right: 12, zIndex: 1000, transform: [
-            {
-              scale: this.state.animatePress
-            }
-          ]}}>
+      <TouchableWithoutFeedback
+        {...this.props}
+        key={iterator}
+        onPressIn={this.animateIn}
+        onPressOut={this.animateOut}
+      >
+        <Animated.View
+          style={{
+            ...styles.button,
+            ...this.props.style,
+            position: 'absolute',
+            bottom: 30,
+            right: 12,
+            zIndex: 1000,
+            transform: [
+              {
+                scale: this.state.animatePress,
+              },
+            ],
+          }}
+        >
           {this.props.children}
         </Animated.View>
       </TouchableWithoutFeedback>
-    )
+    );
   }
 }
 
@@ -57,7 +74,5 @@ const styles = StyleSheet.create({
     },
     shadowRadius: 5,
     shadowOpacity: 0.3,
-
-  }
-
-})
+  },
+});
