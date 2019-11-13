4bbf8586df390d7e4b03000d020d6966358ccda9
Vincent de Almeida <vince0656@gmail.com>
2019-11-13 16:59:40 +0000

    Added further logging to trigger function

--- src/Apps/CheezeWizards/Lambda/functions/index.js
+++ src/Apps/CheezeWizards/Lambda/functions/index.js
@@ -1,9 +1,15 @@
 const functions = require('firebase-functions');
-const apps = require('../../../../AliceSDK/Firebase/index');
+const admin = require('firebase-admin');
+admin.initializeApp({
+    credential: admin.credential.cert(require('./_keys/adminsdk-keys')),
+    databaseURL: 'https://alice-1555232535074.firebaseio.com'
+});
 
+const messaging = admin.messaging();
+//messaging.usePublicVapidKey('BC2C0XVu6eA5fQtHA-HUz9JaBFsVdC-lVutxyMbOuTHDXP8Rm0nGJinEitz2KdV97lgKCoYyOr_fSBP1MJ74A88');
 exports.challengeNotifier = functions.firestore
     .document('wizards/network/{network}/{wizardId}/duel/{challengeId}')
-    .onWrite((change, context) => {
+    .onWrite(async (change, context) => {
         // Get an object with the current document value.
         // If the document does not exist, it has been deleted.
         const document = change.after.exists ? change.after.data() : null;
@@ -18,11 +24,27 @@ exports.challengeNotifier = functions.firestore
         console.log('challenge id', context.params.challengeId);
 
         // Send message using Firebase Cloud Messaging
+        console.log('Constructing a FCM payload...');
         const payload = {
             notification: {
                 title: 'Hello FCM',
                 body: `${context.params.network}-${context.params.wizardId}-${context.params.challengeId}`
             }
         };
-        return apps.messaging.sendToTopic('challenge', payload);
+        console.log('notification.title', payload.notification.title);
+        console.log('notification.body', payload.notification.body);
+
+        const response = await messaging.sendToTopic('challenge', payload, {
+            priority: "high",
+            timeToLive: 60
+        });
+        // response.results.forEach((result, index) => {
+        //     const error = result.error;
+        //     if (error) {
+        //         console.error('Failure sending notification', error);
+        //     }
+        // });
+        console.log('response', response);
+
+        return Promise.resolve('done');
     });
