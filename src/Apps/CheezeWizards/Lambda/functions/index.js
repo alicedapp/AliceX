db9e420d5b96e1e9d1de0e45b25bcc6a79dfc44e
Vincent de Almeida <vince0656@gmail.com>
2019-11-14 14:50:40 +0000

    Can work out new challenges, deletes and updates for context aware notifications

--- src/Apps/CheezeWizards/Lambda/functions/index.js
+++ src/Apps/CheezeWizards/Lambda/functions/index.js
@@ -6,43 +6,105 @@ admin.initializeApp({
 });
 
 const messaging = admin.messaging();
+
+function notifyChallengeRevoked(registrationToken, wizardId, network) {
+    const payload = {
+        token: registrationToken,
+        notification: {
+            title: 'Duel Challenge Revoked!',
+            body: `[[${network}]] Your wizard (${wizardId}) is no longer challenged to a duel`
+        },
+        data: {
+            wizardId
+        }
+    };
+    return messaging.send(payload);
+}
+
+function notifyNewChallengeReceived(registrationToken, wizardId, network) {
+    const payload = {
+        token: registrationToken,
+        notification: {
+            title: 'New Duel Challenge!',
+            body: `[[${network}]] Your wizard (${wizardId}) has been challenged to a duel`
+        },
+        data: {
+            wizardId
+        }
+    };
+    return messaging.send(payload);
+}
+
+function notifyChallengeUpdated(registrationToken, wizardId, network) {
+
+}
+
+function notifyChallengedWizard(document, notifyFn, registrationToken, wizardId, network) {
+    console.log('Asserting not challenger...');
+    let processor = Promise.resolve('done');
+    if (!document.challenger) {
+        console.log('Proceeding as not the challenging wizard');
+        processor = notifyFn(registrationToken, wizardId, network);
+        console.log('Notifying the owner of the challenged wizard');
+    } else {
+        console.log('Finishing as this is the challenging wizard');
+    }
+    return processor;
+}
+
+//todo: this is temp - remove it
+const registrationToken = 'esrFsgLnRB4l8NHuQrpilt:APA91bE_vxfYgOrvY-Vp8FXHrRBBoxRK76pCJpZxap8lYtgPPZXiHsgwTHkbB2RnqGxSn34LyGGU16LPiNXzPzfuMXTolJpqCKtNWmWO8kMpfq-IJ-xfWpdhiiN1Vo14JpM9xui_aMdQ';
 exports.challengeNotifier = functions.firestore
     .document('wizards/network/{network}/{wizardId}/duel/{challengeId}')
     .onWrite(async (change, context) => {
-        // Get an object with the current document value.
-        // If the document does not exist, it has been deleted.
-        const document = change.after.exists ? change.after.data() : null;
+        console.log('**Executing challenge notifier**');
 
-        // Get an object with the previous document value (for update or delete)
+        const network = context.params.network;
+        const wizardId = context.params.wizardId;
+        const challengeId = context.params.challengeId;
+        console.log(`Context params: Network - ${network}, Wizard ID: ${wizardId}, Challenge ID: ${challengeId}`);
+
+        // Todo: fetch this based on the owner of the wizard. For now this is hard coded
+        console.log('registrationToken', registrationToken);
+
+        const document = change.after.exists ? change.after.data() : null;
         const oldDocument = change.before.data();
 
-        // perform desired operations ...
-        console.log('was delete?', document === null);
-        console.log('network', context.params.network);
-        console.log('wizard id', context.params.wizardId);
-        console.log('challenge id', context.params.challengeId);
+        // promise returned from the function
+        let processor = null;
 
-        // Send message using Firebase Cloud Messaging
-        console.log('Constructing a FCM payload...');
-        const registrationToken = 'd6OLxN1_tYxeIQKpJa54NG:APA91bEmfEC7ddh9IavojX6ukPhtnqyODhs1qD8Hdv8zqL1CAjr71EiW6N3q2gaLz5BcnuswkuS5KYFZGMgRgcOTUmUREJfS1Yfx5S8JgZk4kgnUhYsSYTA4QGd5tpHDLSuQNab8n2b4';
-        console.log('registrationToken', registrationToken);
-        const payload = {
-            token: registrationToken,
-            notification: {
-                title: 'Hello Mark!',
-                body: `Challenge issued on ${context.params.network}. Wizard ID: ${context.params.wizardId}. Challenge: ${context.params.challengeId}`
-            }
-        };
-        console.log('notification.title', payload.notification.title);
-        console.log('notification.body', payload.notification.body);
-        const response = await messaging.send(payload);
-        // response.results.forEach((result, index) => {
-        //     const error = result.error;
-        //     if (error) {
-        //         console.error('Failure sending notification', error);
-        //     }
-        // });
-        console.log('response', response);
+        // An undefined document means the challenge has been revoked
+        // An undefined oldDocument means that the challenge is new
+        // Anything else is an update to an existing challenge on one or more sides
+        if (!document) {
+            console.log('Challenge revoked');
+            processor = notifyChallengedWizard(
+                oldDocument,
+                notifyChallengeRevoked,
+                registrationToken,
+                wizardId,
+                network
+            );
+        } else if(!oldDocument) {
+            console.log('New challenge received');
+            processor = notifyChallengedWizard(
+                document,
+                notifyNewChallengeReceived,
+                registrationToken,
+                wizardId,
+                network
+            );
+        } else {
+            console.log('Challenge updated');
+            processor = notifyChallengedWizard(
+                oldDocument,
+                notifyChallengeUpdated,
+                registrationToken,
+                wizardId,
+                network
+            );
+        }
 
-        return Promise.resolve('done');
+        console.log('**End of challenge notifier**');
+        return processor;
     });
