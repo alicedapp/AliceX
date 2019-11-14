97d437e7c8408e2842cb565230974b5eff200d99
Vincent de Almeida <vince0656@gmail.com>
2019-11-14 17:07:20 +0000

    Removed messging from firebase config

--- src/AliceSDK/Firebase/index.js
+++ src/AliceSDK/Firebase/index.js
@@ -1,14 +1,10 @@
 const firebase = require('firebase');
 require("firebase/firestore");
-require("firebase/messaging");
-const admin = require('firebase-admin');
 
 const {firebaseConfig} = require('../../../env');
 
-admin.initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 
 module.exports = {
-    db: firebase.firestore(),
-    messaging: admin.messaging(),
+    db: firebase.firestore()
 };
